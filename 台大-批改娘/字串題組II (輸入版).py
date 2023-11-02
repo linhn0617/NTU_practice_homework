df57c04eefcf048d68e2ad1ef5ed0ac3f93837ca
linhn0617 <79580533+linhn0617@users.noreply.github.com>
2023-11-02 17:44:00 +0800

    Add files via upload

---
+++ 台大-批改娘/字串題組II (輸入版).py
@@ -0,0 +1,8 @@
+line1 = input()
+line2 = input()
+line3 = input()
+line4 = input()
+print(line1.split())
+print(line2.count("a"))
+print(line3.find("fly"))
+print(line4.replace("z","o").replace("x","a"))
